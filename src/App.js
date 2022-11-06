b49b37b6d8f4c94628d739df39c47eb4365da505
Rohit Gupta <lucidrohit@gmail.com>
2022-11-07 00:56:49 +0530

    added boilerplate and routing for website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import BoilerPlate from "./components/boilerPlate/boilerPlate";
+import About from "./pages/about/about";
+import Community from "./pages/community/community";
+import Events from "./pages/events/events";
+import Home from "./pages/home/home";
+import Team from "./pages/team/team";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<BoilerPlate />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
