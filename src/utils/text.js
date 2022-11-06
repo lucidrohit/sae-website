9e4e1a630849d54968115f8c708445a1092c17e0
Rohit Gupta <lucidrohit@gmail.com>
2022-11-06 23:47:33 +0530

    function to create first word red

---
+++ src/utils/text.js
@@ -0,0 +1,13 @@
+export function firstWordStyle(text){
+    return text.trim().split(" ").map((item, index) => {
+        item = item + " "
+        if (index === 0) return <span
+            key={index}
+            className="heading__first">
+            {item}
+        </span>
+        return item  
+    })
+}
+
+
