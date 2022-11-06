f5ef26adb1afbc650d10f0164624070bbb5791c5
Rohit Gupta <lucidrohit@gmail.com>
2022-11-06 23:51:42 +0530

    added user profile component

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,13 +1,13 @@
 import "./userProfile.scss";
 
-function UserProfile({ name = "Jack Harlow", logo }) {
+function UserProfile({ name = "SAE India", logo ,heading="NIT Srinagar"}) {
   return (
     <div className="userProfile sideBar__box">
       <div className="userProfile__logo">
-        {/* <img src={logo} alt="user" /> */}
+        <img src={logo} alt="user" />
       </div>
       <div className="userProfile__details">
-        <h3 className="userProfile__heading">Username</h3>
+        <h3 className="userProfile__details__heading">{heading}</h3>
         <p className="userProfile__details__name">{name}</p>
       </div>
     </div>
