c18c842f42c73a5576986d9f0fa52eb608c57889
Rohit Gupta <lucidrohit@gmail.com>
2022-11-12 03:04:34 +0530

    made first word style to multi word style

--- src/components/common/headingText/headingText.jsx
+++ src/components/common/headingText/headingText.jsx
@@ -1,8 +1,8 @@
 import "./headingText.scss";
 import { firstWordStyle } from "../../../utils/text.js";
 
-function HeadingText({ text }) {
-  const data = firstWordStyle(text);
+function HeadingText({ text, wordHights = 1 }) {
+  const data = firstWordStyle(text, wordHights);
   return <h2 className="heading">{data}</h2>;
 }
 
