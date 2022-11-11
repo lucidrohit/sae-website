38ba07f21002d298371c1cc309aaadca0d304644
Rohit Gupta <lucidrohit@gmail.com>
2022-11-12 03:05:43 +0530

    added home page content and about redirect functionality

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,13 @@
 import Button from "../../components/common/button/button";
 import IconText from "../../components/common/iconText/iconText";
 import "./home.scss";
-import SaeLogo from "../../assets/sae.webp"
+import SaeLogo from "../../assets/sae.webp";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const navigateToAbout = () => navigate("/about");
+
   return (
     <div className="home">
       <div className="home__about__iconText">
@@ -11,17 +15,16 @@ function Home() {
       </div>
       <div className="home__about__details">
         <div className="home__about__details__heading">
-          Lorem ipsum dolor sit amet ?
+          PURPOSE OF AN SAEINDIA COLLEGIATE CLUB ?
         </div>
         <div className="home__about__details__para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, sint
-          officiis numquam aliquid corrupti illo? Nesciunt aliquam hic
-          inventore? Laboriosam, exercitationem nemo tempora aspernatur officia
-          enim eaque quibusdam magnam, rem dolorem asperiores quia quasi, cumque
-          itaque fugit ea! Error repellat similique voluptate eius est animi
-          iusto nisi accusantium aperiam obcaecati!
+          Successful SAEINDIA Collegiate clubs provide many benefits to their
+          members and are assets to the engineering colleges where they are
+          located. Collegiate clubs provide students with tangible contact with
+          their future profession – engineering – and, in so doing, further the
+          objectives of engineering...
         </div>
-        <Button text={"Read More"} />
+        <Button text="Read More" onClick={navigateToAbout} />
       </div>
     </div>
   );
